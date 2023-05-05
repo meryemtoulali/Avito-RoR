fc555f8756c65d3382c9a440c15908aeb056ab8b
sweatyfbiprofiler <toulalimeryem@gmail.com>
2023-05-05 14:24:27 +0100

    added icons to carousel sections

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ before_action :set_carousels, only: %i[ home ]
     def set_carousels
       @annonces_immobilier = Annonce.select { |annonce| annonce.category.id == 3 }.first(10)
       @annonces_vehicules = Annonce.select { |annonce| annonce.category.id == 2 }.first(10)
-      @annonces_loisirs = Annonce.select { |annonce| annonce.category.id == 6 }.first(10)
+      @annonces_tech = Annonce.select { |annonce| annonce.category.id == 1 }.first(10)
 
 
     end
